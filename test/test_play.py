2ec22c9527dc01f73aef3e9a666b8095104ad203
Atompichapop <atompaewpolsong@gmail.com>
2023-11-29 13:57:43 +0700

    Update test_play.py

--- test/test_play.py
+++ test/test_play.py
@@ -9,7 +9,7 @@ from chess.pgn import Game
 
 opponent = agent.GreedyAgent()
 env = environment.Board(opponent, FEN=None)
-player = agent.Agent(lr=0.001, network='big')
+player = agent.Agent(learning_rate=0.001, network='big')
 player.fix_model()
 learner = learn.TD_search(env, player, gamma=0.8, search_time=1.5)
 node = tree.Node(learner.env.board, gamma=learner.gamma)
